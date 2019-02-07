5ab4b3b094bbb50063c8478d028deb9e410dcd16
David Baker <34483246+DavidDBaker@users.noreply.github.com>
2019-02-07 06:03:52 -0500

    removed some info

--- routes/search.js
+++ routes/search.js
@@ -2,8 +2,8 @@
 
 const request = require('request');
 
-const apiKey = 'e8d2a1ed1c8f49639d03b3f7eff8d85d';
-const customConfigId = '9760f4dc-5d3e-47a8-8797-91ded37df33f';
+const apiKey = 'key';
+const customConfigId = 'customConfigId';
 
 function searchQuery(query) {
   let options = {
@@ -11,7 +11,7 @@ function searchQuery(query) {
       `https://api.cognitive.microsoft.com/bingcustomsearch/v7.0/search?q=${encodeURIComponent(query)}&customconfig=${customConfigId}`,
   
     headers: {
-      'Ocp-Apim-Subscription-Key' : 'e8d2a1ed1c8f49639d03b3f7eff8d85d'
+      'Ocp-Apim-Subscription-Key' : apiKey
     }
   };
   request(options, (error, response, body) =>{
